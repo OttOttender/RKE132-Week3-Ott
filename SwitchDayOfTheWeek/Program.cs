9d1c99c21b17804c0e66145bfcdf5ab1c7aae1b3
Ottomann <ott.ottender@gmail.com>
2023-09-23 16:18:33 +0300

    add week3 code

---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+//switch kui v22rtus on kindel number (v2i ka avaldis), mida on rohkem kui kaks
+switch (weekDay) 
+{
+case 0:
+        Console.WriteLine("It's Sunday!");
+        break;
+    case 1:
+        Console.WriteLine("It's Monday!");
+        break;
+    case 2:
+        Console.WriteLine("It's Tuesday!");
+        break;
+    case 3:
+        Console.WriteLine("It's Wednesday!");
+        break;
+    case 4:
+        Console.WriteLine("It's Thursday!");
+        break;
+    case 5:
+        Console.WriteLine("It's Friday!");
+        break;
+    case 6:
+        Console.WriteLine("It's Saturday!");
+        break;
+        default: Console.WriteLine("Oops. Your calendar might be broken.");
+        break;
+}       
+
+Console.WriteLine("Have a nice day!");
---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,8 @@
+﻿// % modulus on jääk, kui 8 küpsist 24 inimesele ss poolitamise asemel jääb 8 tervet alles
+int a = 100;
+int b = 2;
+
+int c = a % b;
+
+Console. WriteLine(c);
+//100 vastuseks 0 kuna 2 mahub 50 korda ning j22k on 0
